7dce7f8e6dcfd5431e828a1fab5eaef0436ef2b8
Lawrence Caulker <locaulker@gmail.com>
2018-07-18 12:07:24 -0400

    created components

---
+++ src/globals/footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="container text-center">
+    <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>
+      <a href="#">Back to top</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import nav from './globals/nav.vue';
+import Footer from './globals/footer.vue';
+import PostExcerpt from './posts/post-excerpt.vue';
+
+Vue.component('app-main-nav', nav);
+Vue.component('app-footer', Footer);
+Vue.component('app-post-excerpt', PostExcerpt);
 
 new Vue({
   el: '#app',
