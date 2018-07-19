5baf15e7f7e944bf14b2a519d0eef1c9c6921723
Lawrence Caulker <locaulker@gmail.com>
2018-07-19 15:42:18 -0400

    passing data to components

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,7 @@
       <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
 
-          <app-post-excerpt></app-post-excerpt>
-          <app-post-excerpt></app-post-excerpt>
-          <app-post-excerpt></app-post-excerpt>
+          <app-post-excerpt v-for="post in posts" :key="post.id" :post="post"></app-post-excerpt>
 
           <nav>
             <ul class="pager">
@@ -29,10 +27,19 @@
 
 <script>
 export default {
-  
+  data: function() {
+    return {
+      posts:    []
+    }
+  },
+  mounted: function() {
+    // var self = this;
+   jQuery.get(wp_rest_api.base_url + 'posts').always((response) => {
+      this.posts    = response;
+    });
+  }
 }
 </script>
 
 <style>
-
 </style>
